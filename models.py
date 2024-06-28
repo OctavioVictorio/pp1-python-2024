09678c0af46dc243ac26985b74d121800f8b1e5c
octavio <octavionvictorio@gmail.com>
2024-06-27 21:27:08 -0300

    agreamos todo

---
+++ models.py
@@ -0,0 +1,32 @@
+from app import db
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+
+    def __str__(self):
+        return self.nombre
+    
+class Tipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(30), nullable=False)
+
+    def __str__(self):
+        return self.nombre
+    
+class Vehiculo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    modelo = db.Column(db.String(30), nullable=False)
+    cilindrada = db.Column(db.Integer)
+
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
+    #marca = db.relationship('Marca', backref=db.backref('vehiculos', lazy=True)) 
+
+    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'), nullable=False)
+    #tipo = db.relationship('Tipo', backref=db.backref('vehiculos', lazy=True))  
+
+    tipo = db.relationship('Tipo', backref= db.backref('vehivulo', lazy=True))
+    marca = db.relationship('Marca', backref= db.backref('vehivulo', lazy=True))
+
+    def __str__(self):
+        return self.modelo, self.cilindrada
